2b14c34379aafb274bb6d66861228f54451421a2
Gabriel Coronado <gabrielomar2809@gmail.com>
2018-08-06 16:31:08 -0400

    Sending specific info

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ArticlesController < ApplicationController
       def index
-        articles = Article.all
+        articles = Article.select(:id, :title, :body)
         render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles }, status: 200
       end
     end
