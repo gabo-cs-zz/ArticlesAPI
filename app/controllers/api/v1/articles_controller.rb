fa5bf56fdc60d09f43d6646abaf551151e81b39f
Gabriel Coronado <gabrielomar2809@gmail.com>
2018-08-08 19:20:05 -0400

    Methods added, controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticlesController < ApiController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :destroy]
   def index
     @articles = Article.all
   end
@@ -7,7 +7,28 @@ class Api::V1::ArticlesController < ApiController
   def show
   end
   
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render :show
+    else
+      render json: {data: article.errors, status: :unprocessable_entity }
+    end
+  end
+  
+  def destroy
+    if @article.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+  
   private
+  def article_params
+    params.permit(:title, :body)
+  end
+
   def set_article
     @article = Article.find(params[:id])
   end
