5a0b2670181364dff1e3cb5c4b0beea86f89e888
Gabriel Coronado <gabrielomar2809@gmail.com>
2018-08-12 14:01:24 -0400

    CORS headers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,15 @@
 class ApiController < ApplicationController
-  before_action :set_default_format
-  
+  before_action :set_default_format, :set_cors
+
   private
   def set_default_format
     request.format = :json
+  end
+  
+  def set_cors
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, PATCH, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
